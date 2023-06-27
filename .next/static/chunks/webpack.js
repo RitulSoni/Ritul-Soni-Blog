77ce6086b0af1e63ced65579f26b74157b341df4
Ritul Soni <rsoni27@illinois.edu>
2023-06-26 20:13:19 -0500

    fixed the blog bug

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -180,7 +180,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "1326f342e19a5107"; }
+/******/ 		__webpack_require__.h = function() { return "66424c64116ad906"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
